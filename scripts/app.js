ced20df3207eda140680f87ce1384dee79ba5eaa
Alfa Limonada <30637998+Alfa-limonada@users.noreply.github.com>
2020-04-02 20:04:43 -0500

    Crear usuarios

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,29 @@
-function crearUsuario() {
-    //variables
-    const users = 0; 
 
-    //métodos
+let userList = [];
+//Array con los usuarios registrados
 
-    //listeners
-};
+const createUser = (ev)=>{
+    ev.preventDefault(); //No enviar info si está vacío
+    let user = {
+        username : document.getElementById("username").value,
+        password : document.getElementById("password").value,
+        remember : document.getElementById("remember").value 
+    }; //Objeto usuario
+
+    if (username.value.length < 1) return;
+    userList.push(user);
+    document.querySelector('form').reset(); //Push objeto user al array
+
+    console.warn('added' , {userList} );
+    let pre = document.querySelector('#msg pre');
+    pre.textContent = '\n' + JSON.stringify(userList, '\t', 2); //Imprimir temporalmente cada array en pantalla
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{ //DomContentLoaded - ya cargó todo
+    document.getElementById('btnCrear').addEventListener('click', createUser); //Cunado de click al botón accionar createUser
+}); // 
+
+
+
+
 
